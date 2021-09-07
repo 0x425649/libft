1406961d3b067c498ae8c8caab02f9737d784953
ffmiyo <leffmiyo@gmail.com>
2021-09-07 20:11:53 +0800

    implement strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,6 +1,26 @@
 #include "libft.h"
 
-// char	*ft_strnstr(const char	*big, const char *little, size_t len)
-// {
-// 	return(NULL);
-// }
+// Find the first substring occurence in a length-limited string.
+// This implementation count little length, i. strncmp haystack against
+// needle as long as len is greater or equal to i.
+char	*ft_strnstr(const char	*big, const char *little, size_t len)
+{
+	size_t			i;
+	char			*needle;
+	char			*haystack;
+
+	haystack = (char *)big;
+	needle = (char *)little;
+	if (*needle == '\0')
+		return (haystack);
+	i = 0;
+	while (*little++)
+		++i;
+	while (len-- >= i)
+	{
+		if (!ft_strncmp(haystack, needle, i))
+			return (haystack);
+		++haystack;
+	}
+	return (NULL);
+}
