d55bc861ad96bfd109ddfa7b18be33246ef29fb0
ffmiyo <leffmiyo@gmail.com>
2021-09-08 03:48:01 +0800

    implement ft_memchr

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,7 +1,8 @@
 #include "libft.h"
 
 // Copy n bytes from src to dst.
-// Undefined behavior for overlap.
+// Undefined behavior for overlap. i.e if memory overlap and dst is in
+// higher memory region, this will copy the same byte over.
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	char unsigned		*ds;
