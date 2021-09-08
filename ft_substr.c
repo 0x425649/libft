cfdae1859f80d6b575b71bfaac76260cea874f82
ffmiyo <leffmiyo@gmail.com>
2021-09-08 13:25:52 +0800

    substr: fix for case start exceeds len

--- ft_substr.c
+++ ft_substr.c
@@ -1,20 +1,24 @@
 #include "libft.h"
 
-// Allocates (with malloc(3)) and returns a substring from the string ’s’.
-// The substring begins at index ’start’ and is of maximum lenght ’len’.
+// Allocates (with malloc(3)) and returns a substring from the string 's'.
+// The substring begins at index 'start' and is of maximum lenght 'len'.
+// Note: return empty string when 'start' exceeds string length.
+//		 this implementation do this implicitly by ft_strlcpy of size 1.
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
-	size_t	cap;
+	size_t	rlen;
 
-	cap = 0;
-	while (s[cap])
-		++cap;
-	if (len < cap)
-		cap = len;
-	cap += 1;
-	if (!(sub = (char *)malloc(sizeof(char) * cap)))
-		return (NULL);;
-	ft_strlcpy(sub, s + start, cap);
+	rlen = 0;
+	while (s[rlen])
+		++rlen;
+	if (len < rlen)
+		rlen = len;
+	if (start > rlen)
+		rlen = 0;
+	sub = (char *)malloc(sizeof(char) * (rlen + 1));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, s + start, rlen + 1);
 	return (sub);
 }
