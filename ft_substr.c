8f3a3e6dd85cf60f70a4dc617cc6741c76717460
ffmiyo <leffmiyo@gmail.com>
2021-09-08 12:53:55 +0800

    substr: fix to account for bounded by max lenght

--- ft_substr.c
+++ ft_substr.c
@@ -1,13 +1,20 @@
 #include "libft.h"
 
 // Allocates (with malloc(3)) and returns a substring from the string ’s’.
-// The substring begins at index ’start’ and is of maximum size ’len’.
+// The substring begins at index ’start’ and is of maximum lenght ’len’.
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
+	size_t	cap;
 
-	if (!(sub = (char *)malloc(sizeof(char) * (len + 1))))
+	cap = 0;
+	while (s[cap])
+		++cap;
+	if (len < cap)
+		cap = len;
+	cap += 1;
+	if (!(sub = (char *)malloc(sizeof(char) * cap)))
 		return (NULL);;
-	ft_strlcpy(sub, s + start, len + 1);
+	ft_strlcpy(sub, s + start, cap);
 	return (sub);
 }
