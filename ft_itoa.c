20b757932c5bca89398186ef99827d1d4d5b102b
ffmiyo <leffmiyo@gmail.com>
2021-09-09 14:26:16 +0800

    itoa: use negative value as default

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,55 +1,50 @@
 #include "libft.h"
 
-static int count_digit(int n)
+static int	count_digit(int n)
 {
-	int 	i;
-	long	num;
+	int	i;
 
-	num = 0;
+	i = 0;
 	if (n == 0)
 		return (1);
-	if (n < 0)
-		num -= n;
-	else
-		num = n;
-	while (num > 0)
+	if (n > 0)
+		n = -n;
+	while (n < 0)
 	{
-		i++;
-		num /= 10;
+		++i;
+		n /= 10;
 	}
-	return i;
+	return (i);
 }
 
 // Allocates (with malloc(3)) and returns a string representing 
 // the integer received as an argument. Negative numbers must be handled.
+// int is normalize into negative to process.
 char	*ft_itoa(int n)
 {
 	int		len;
 	char	*str;
 	int		is_neg;
-	long	num;
 
-	is_neg = 0;
-	len = count_digit(n);
-	if (n < 0)
+	is_neg = 1;
+	len = 1;
+	len += count_digit(n);
+	if (n >= 0)
 	{
-		++len;
-		is_neg = 1;
-		num = 0;
-		num -= n;
+		--len;
+		is_neg = 0;
+		n = -n;
 	}
-	else
-		num = n;
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
 	str[len] = '\0';
 	if (n == 0)
 		*str = '0';
-	while (num > 0)
+	while (n < 0)
 	{
-		str[--len] = (num % 10) + '0';
-		num /= 10;
+		str[--len] = -(n % 10) + '0';
+		n /= 10;
 	}
 	if (is_neg)
 		*str = '-';
