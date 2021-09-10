938c43f002830e5237a8b69a74d8e299132846ed
ffmiyo <leffmiyo@gmail.com>
2021-09-10 16:39:41 +0800

    implement strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+// Apply function f to all characters in string s.
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*str;
+
+	if (s == 0 || f == 0)
+		return (NULL);
+	str = malloc(sizeof(*s) * (ft_strlen(s) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
