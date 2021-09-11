c41d145d533820bf9b2366012c29b295b4a42b4f
ffmiyo <leffmiyo@gmail.com>
2021-09-11 16:04:36 +0800

    implement lstiter

---
+++ ft_lstiter.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+// Iteratively apply function f to cotent of each element in the list.
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (lst == NULL || f == NULL)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
